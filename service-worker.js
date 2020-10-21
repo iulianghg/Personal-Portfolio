dc9b8b04bc4a2cde134594ebd7a57edbcf9069f2
Andrei-Ilici <69673987+Andrei-Ilici@users.noreply.github.com>
2020-10-21 12:54:11 +0000

    Deploying to gh-pages from  @ e9185c24d8c8823cae04bce61b5b26fda66922ce 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ec5eeaf105fc809a086447100f952a6.js"
+  "/portfolio/precache-manifest.28ca8d9039b2133e9b2940b7d21dda56.js"
 );
 
 self.addEventListener('message', (event) => {
